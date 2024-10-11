82271c120d0926a3690020bdc1d661affcbd96ef
Roope Järvinen <roope.jarvinen@nokia.com>
2024-10-11 14:46:20 +0300

    Formatting

--- dps_control.py
+++ dps_control.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 import time
-import minimalmodbus
 from enum import IntEnum
 from serial import SerialException
+import minimalmodbus
 from minimalmodbus import ModbusException
 
 # Configuration file
@@ -17,20 +17,23 @@ minimalmodbus.MODE_RTU = 'rtu'
 minimalmodbus.BAUDRATE = 19200
 minimalmodbus.TIMEOUT = 0.5
 minimalmodbus.CLOSE_PORT_AFTER_EACH_CALL = False
-instrument = None
+INSTRUMENT = None
 DEBUG = True
 
 class ValueType(IntEnum):
+    """Type of a value"""
     INT = 1
     FLOAT = 2
 
 class Register(IntEnum):
+    """Register entries"""
     VOLTS = 0x0
     AMPS = 0x1
 
 # Decode modbus status response
 def decode_modbus_status_response(b):
-    if (len(b) != 20):
+    """Decode Modbus response and return printable structure"""
+    if len(b) != 20:
         print('Invalid Modbus response')
         return
 
@@ -53,37 +56,42 @@ def decode_modbus_status_response(b):
 
 # Set device voltage
 def set_voltage(volts):
+    """Set voltage of DPS device"""
     if (volts > conf.max_voltage or volts < conf.min_voltage):
         print('Voltage must be between %d and %d' % (conf.min_voltage, conf.max_voltage))
         return
-    instrument.write_register(Register.VOLTS, value=volts, number_of_decimals=2)
+    INSTRUMENT.write_register(Register.VOLTS, value=volts, number_of_decimals=2)
 
 
 # Set device current
 def set_current(amps):
+    """Set current of DPS device"""
     if (amps > conf.max_current or amps < conf.min_current):
         print('Current must be between %d and %d' % (conf.min_current, conf.max_current))
         return
-    instrument.write_register(Register.AMPS, value=amps, number_of_decimals=3)
+    INSTRUMENT.write_register(Register.AMPS, value=amps, number_of_decimals=3)
 
 # Read registers for status
 def read_registers():
-    registers = instrument.read_registers(registeraddress=0x0, number_of_registers=20)
+    """Read all registers of DPS device"""
+    registers = INSTRUMENT.read_registers(registeraddress=0x0, number_of_registers=20)
     return registers
 
 
 # Print command prompt and return input
-def commandPrompt():
+def command_prompt():
+    """Get command from prompt"""
     cmd = input('DPS: ')
     return cmd
 
 # Check that value can be converted to number of certain type
 def validateInput(val, valtype):
+    """Check that value in correct format"""
     ret = True
 
-    if (valtype == ValueType.FLOAT):
+    if valtype == ValueType.FLOAT:
         val = float(val)
-    elif (valtype == ValueType.INT):
+    elif valtype == ValueType.INT:
         val = int(val)
     else:
         print('Invalid value')
@@ -91,7 +99,8 @@ def validateInput(val, valtype):
     return ret
 
 # Print some help
-def printHelp():
+def print_help():
+    """Print generic help for commands available"""
     print('dps-control v%s' % (VERSION))
     print('-----------------------------------------------------------')
     print('Available commands:')
@@ -102,28 +111,29 @@ def printHelp():
     print('\tq\t\tQuit program')
 
 # Parse command and execute it
-def parseCommand(cmd):
+def parse_command(cmd):
+    """Parse input command"""
     ret = True
     try:
-        mainCmd = cmd.split()[0].lower()
-        if (mainCmd == 'v'):
+        main_cmd = cmd.split()[0].lower()
+        if main_cmd == 'v':
             volts = cmd.split()[1]
-            if (validateInput(volts, ValueType.FLOAT)):
+            if validateInput(volts, ValueType.FLOAT):
                 volts = "{:.2f}".format(float(volts))
                 msg = "%s %s %s" % ('Set voltage to:', volts, 'V')
                 print(msg)
                 set_voltage(float(volts))
-        elif (mainCmd == 'a'):
+        elif main_cmd == 'a':
             amps = cmd.split()[1]
-            if (validateInput(amps, ValueType.FLOAT)):
+            if validateInput(amps, ValueType.FLOAT):
                 amps = "{:.3f}".format(float(amps))
                 msg = "%s %s %s" % ('Set current to:', amps, 'A')
                 print(msg)
                 set_current(float(amps))
-        elif (mainCmd == 'i'):
+        elif main_cmd == 'i':
             print('Getting info...')
             print(decode_modbus_status_response(read_registers()))
-        elif (mainCmd == 'l'):
+        elif main_cmd == 'l':
             print('Running live monitoring, press [CTRL-C] to stop...')
 
             try:
@@ -135,11 +145,9 @@ def parseCommand(cmd):
                     time.sleep(1)
             except KeyboardInterrupt:
                 print('\n')
-                pass
-
-        elif (mainCmd == 'h'):
-            printHelp()
-        elif (mainCmd == 'q'):
+        elif main_cmd == 'h':
+            print_help()
+        elif main_cmd == 'q':
             ret = False
         return ret
     except (TypeError, ValueError, ModbusException, SerialException) as error:
@@ -149,12 +157,12 @@ def parseCommand(cmd):
 
 
 def initialize():
-    # Initialize Modbus
+    """Initialize Modbus"""
     print('Initializing...')
-    global instrument
+    global INSTRUMENT
     try:
-        instrument = minimalmodbus.Instrument(port=conf.ttyDevice, slaveaddress=conf.slave)
-        instrument.debug = DEBUG
+        INSTRUMENT = minimalmodbus.Instrument(port=conf.ttyDevice, slaveaddress=conf.slave)
+        INSTRUMENT.debug = DEBUG
     except (TypeError, ValueError, ModbusException, SerialException) as error:
         print(error)
         return False
@@ -163,17 +171,18 @@ def initialize():
 
 # Main program
 def main():
+    """Main program"""
     running = True
 
-    if (initialize() != True):
+    if not initialize():
         print('ERROR: Cannot initialize Modbus')
         return
 
     # Main loop
     while running:
-        cmd = commandPrompt()
-        if (len(cmd)):
-            running = parseCommand(cmd)
+        cmd = command_prompt()
+        if len(cmd):
+            running = parse_command(cmd)
 
 if __name__ == "__main__":
     main()
