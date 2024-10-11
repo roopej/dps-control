86c4fbac00a8568f33b75075270bcebaee8c10eb
Roope Järvinen <roope.jarvinen@nokia.com>
2024-10-11 12:47:57 +0300

    Initial commit.

---
+++ dps_config.py
@@ -0,0 +1,11 @@
+# TTY device to use for communication
+ttyDevice='/dev/ttyS0'
+
+# Slave number of the DPS device
+slave=1
+
+# Min and max settings allowed for voltage and current
+max_voltage = 50
+min_voltage = 0
+max_current = 5
+min_current = 0
