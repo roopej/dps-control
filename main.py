90f12104355491ee730fdd516f25d5ffbbacb842
Roope Järvinen <roope.jarvinen@outlook.com>
2024-10-15 22:59:38 +0300

    Two UIs

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 """CLI Interface for DPS Control"""
-
+import sys
 from queue import SimpleQueue
 from dps_controller import DPSController
 from dps_cli import DPSCli
+from dps_gui import launch_gui
 
 def main():
     """Entry point to DPS Controller"""
+    # Arguments
+    args: str = sys.argv
 
     # Create event queue
     eventq: SimpleQueue = SimpleQueue()
@@ -13,9 +16,11 @@ def main():
     # Create controller
     controller = DPSController(eventq)
 
-    # Create view and pass controller to it and start
-    cli = DPSCli(controller)
-    cli.start()
-
+    if len(args) > 1 and args[1] == '--cli':
+        # Create view and pass controller to it and start
+        cli = DPSCli(controller)
+        cli.start()
+    else:
+        launch_gui()
 if __name__ == "__main__":
     main()
