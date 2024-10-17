cbef1fa5ba02f3e2ba70b1715ef2914817e679ef
Roope Järvinen <roope.jarvinen@nokia.com>
2024-10-17 15:43:57 +0300

    Testing dialbar changes.

--- custom_widgets/dialbar.py
+++ custom_widgets/dialbar.py
@@ -3,15 +3,16 @@ from PySide6.QtCore import Qt
 from utils import get_button, get_label, get_lineedit
 
 class _Bar(QtWidgets.QWidget):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.setSizePolicy(
             QtWidgets.QSizePolicy.MinimumExpanding,
             QtWidgets.QSizePolicy.MinimumExpanding
         )
 
-    def sizeHint(self):
-        return QtCore.QSize(40,120)
+    def sizeHint(self) -> QtCore.QSize:
+        return QtCore.QSize(30,120)
+
     def paintEvent(self, e):
         painter = QtGui.QPainter(self)
         brush = QtGui.QBrush()
@@ -19,6 +20,23 @@ class _Bar(QtWidgets.QWidget):
         brush.setStyle(Qt.SolidPattern)
         rect = QtCore.QRect(0, 0, painter.device().width(), painter.device().height())
         painter.fillRect(rect, brush)
+
+        # Get current state.
+        dial = self.parent()._dial
+        vmin, vmax = dial.minimum(), dial.maximum()
+        value = dial.value()
+
+        pen = painter.pen()
+        pen.setColor(QtGui.QColor('red'))
+        painter.setPen(pen)
+
+        font = painter.font()
+        font.setFamily('Times')
+        font.setPointSize(18)
+        painter.setFont(font)
+
+        painter.drawText(25, 25, "{}-->{}<--{}".format(vmin, value, vmax))
+        painter.end()
 
 class DialBar(QtWidgets.QWidget):
     """Combination of dial and vertical bar for settings values"""
