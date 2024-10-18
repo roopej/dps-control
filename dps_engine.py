64aefa2ab18d6f8051b0a88329993df435c14641
Roope Järvinen <roope.jarvinen@outlook.com>
2024-10-18 17:38:12 +0300

    WIP

--- dps_engine.py
+++ dps_engine.py
@@ -9,6 +9,30 @@ from minimalmodbus import ModbusException
 from serial import SerialException
 from enum import IntEnum
 
+# Return codes and messages
+class DPSRetCode(IntEnum):
+    """Return code enumerations from engine"""
+    DPS_OK = 0x0
+    DPS_ERROR = 0x1
+
+class DPSRet:
+    """Return status class from engine"""
+    def __init__(self, code: DPSRetCode, value: str = '',  msg: str = '') -> None:
+        self.code = code
+        self.value = value
+        self.msg = msg
+
+    def get_value(self):
+        """Get only the value of return code"""
+        return self.value
+
+    def get_message(self):
+        """Get only the message of return code"""
+        return self.msg
+
+    def __repr__(self) -> str:
+        """Representation of return code"""
+        return f'code: {self.code}, value: {self.value}, message: {self.msg}'
 
 class DPSRegister(IntEnum):
     """Register addresses of DPS5005"""
@@ -37,20 +61,20 @@ def exception_handler(func):
 
 class DPSEngine:
     """Class interacting with DPS5005 through Modbus protocol"""
-    def __init__(self, port: str, slave: int, debug : bool = False):
+    def __init__(self, port: str, slave: int, debug : bool = False) -> None:
         """Constructor"""
         self.port = port
         self.instrument = None
         self.slave = slave
         self.debug = debug
 
-    def connect(self) -> bool:
+    def connect(self) -> DPSRet:
         """Connect to DPS through modbus"""
         try:
             self.instrument = minimalmodbus.Instrument(port=self.port, slaveaddress=self.slave)
         except SerialException as error:
             print(error)
-            return False
+            return DPSRet(DPSRetCode.DPS_ERROR, msg = 'Serial exception')
 
         self.instrument.serial.baudrate = 9600
         self.instrument.serial.bytesize = 8
@@ -58,64 +82,68 @@ class DPSEngine:
         self.instrument.mode = minimalmodbus.MODE_RTU
         self.instrument.close_port_after_each_call = False
         self.instrument.debug = self.debug
-        return True
+        return DPSRet(DPSRetCode.DPS_OK)
 
     # Getters and setters
-    def set_power(self, enable: bool) -> None:
+    def set_power(self, enable: bool) -> DPSRet:
         """Switch power output ON/OFF"""
         self.write_register(DPSRegister.PWR_ONOFF, int(enable), 0)
+        DPSRet(DPSRetCode.DPS_OK)
 
-    def get_power(self) -> float:
+    def get_power(self) -> DPSRet:
         """Get current power ON/OFF status"""
         power = self.read_register(DPSRegister.PWR_ONOFF, 0)
-        return power
+        return DPSRet(DPSRetCode.DPS_OK, str(power))
 
-    def toggle_power(self) -> None:
+    def toggle_power(self) -> DPSRet:
         """Toggle power ON<->OFF"""
         current_status = self.get_power()
         self.set_power(not current_status)
+        return DPSRet(DPSRetCode.DPS_OK)
 
-    def set_volts(self, volts: float) -> None:
+    def set_volts(self, volts: float) -> DPSRet:
         """Set voltage of DPS device"""
         #TODO: Limit check
         self.write_register(DPSRegister.VOLTS_SET, volts, 2)
+        return DPSRet(DPSRetCode.DPS_OK)
 
-    def get_volts_set(self) -> float:
+    def get_volts_set(self) -> DPSRet:
         """Get set value of volts out, not necessary the actual out voltage atm"""
         volts = self.read_register(DPSRegister.VOLTS_SET, 2)
-        return volts
+        return DPSRet(DPSRetCode.DPS_OK, str(volts))
 
-    def get_volts_out(self) -> float:
+    def get_volts_out(self) -> DPSRet:
         """Get voltage output at the moment"""
         volts = self.read_register(DPSRegister.VOLTS_OUT, 2)
-        return volts
+        return DPSRet(DPSRetCode.DPS_OK, str(volts))
 
-    def set_amps(self, amps: float) -> None:
+    def set_amps(self, amps: float) -> DPSRet:
         """Set current of DPS device"""
         #TODO: Limit check
         self.write_register(DPSRegister.AMPS_SET, amps, 3)
+        return DPSRet(DPSRetCode.DPS_OK)
 
-    def get_amps_set(self) -> float:
+    def get_amps_set(self) -> DPSRet:
         """Get set value of amps out, not necessary the actual out current atm"""
         volts = self.read_register(DPSRegister.AMPS_SET, 3)
-        return volts
+        return DPSRet(DPSRetCode.DPS_OK, str(volts))
 
-    def get_amps_out(self) -> float:
+    def get_amps_out(self) -> DPSRet:
         """Get current output at the moment"""
-        volts = self.read_register(DPSRegister.AMPS_OUT, 3)
-        return volts
+        amps = self.read_register(DPSRegister.AMPS_OUT, 3)
+        return DPSRet(DPSRetCode.DPS_OK, str(amps))
 
-    def get_power_out(self) -> float:
+    def get_power_out(self) -> DPSRet:
         """Get current power output"""
         power = self.read_register(DPSRegister.PWR_OUT, 2)
-        return power
+        return DPSRet(DPSRetCode.DPS_OK, str(power))
 
     def get_status(self) -> List[int]:
         """Get dump of status variables of DPS, 20 registers starting from 0x0"""
         b = self.read_registers(0x0, 20)
         return b
 
-    # Communication through Modbus
+    # Communication through Modbus, catch exceptions on these, used internally by class
     @exception_handler
     def write_register(self, address: int, value: Union[int,float], num_decimals: int) -> None:
         """Write single register at at address"""
