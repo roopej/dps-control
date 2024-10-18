0165753b8c8b92cfe5e43fc9a864efbecbcf97ac
Roope Järvinen <roope.jarvinen@outlook.com>
2024-10-18 17:38:09 +0300

    Some help texts.

--- dps_config.py
+++ dps_config.py
@@ -5,7 +5,7 @@ ttyDevice='/dev/ttyS0'
 slave=1
 
 # Min and max settings allowed for voltage and current
-max_voltage = 50
-min_voltage = 0
-max_current = 5
-min_current = 0
+max_voltage = 50.00
+min_voltage = 0.00
+max_current = 5.000
+min_current = 0.000
