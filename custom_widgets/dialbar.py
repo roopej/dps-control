697261083bf85c2c2c44a736878e4c253f4dfd28
Roope Järvinen <roope.jarvinen@outlook.com>
2024-10-18 17:38:16 +0300

    Progress

--- custom_widgets/dialbar.py
+++ custom_widgets/dialbar.py
@@ -3,25 +3,40 @@ from PySide6.QtCore import Qt
 from utils import get_button, get_label, get_lineedit
 
 class _Bar(QtWidgets.QWidget):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setSizePolicy(
+            QtWidgets.QSizePolicy.MinimumExpanding,
+            QtWidgets.QSizePolicy.MinimumExpanding
+        )
+
+    def sizeHint(self):
+        return QtCore.QSize(40,120)
+    def paintEvent(self, e):
+        painter = QtGui.QPainter(self)
+        brush = QtGui.QBrush()
+        brush.setColor(QtGui.QColor('black'))
+        brush.setStyle(Qt.SolidPattern)
+        rect = QtCore.QRect(0, 0, painter.device().width(), painter.device().height())
+        painter.fillRect(rect, brush)
 
 class DialBar(QtWidgets.QWidget):
     """Combination of dial and vertical bar for settings values"""
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, unit: str, *args, **kwargs) -> None:
         super(DialBar, self).__init__(*args, **kwargs)
 
         # Main layout
         layout = QtWidgets.QVBoxLayout()
         self._bar = _Bar()
         self._dial = QtWidgets.QDial()
-        self._dial.setNotchesVisible(True)
+        self._dial.setNotchesVisible(False)
         self._dial.setFocusPolicy(Qt.FocusPolicy.NoFocus)
 
         # Display part
         bottom_layout = QtWidgets.QHBoxLayout()
         self.volt_input: QtWidgets.QLineEdit = get_lineedit('0.00', 18, 4, Qt.FocusPolicy.StrongFocus)
         self.volt_input.setMaximumWidth(100)
-        volt_unit_label: QtWidgets.QLabel = get_label('V', 22)
+        volt_unit_label: QtWidgets.QLabel = get_label(unit, 22)
         bottom_layout.addWidget(self.volt_input)
         bottom_layout.addWidget(volt_unit_label)
 
