7e0fbe8b6276c8c89cb1c93669c76d697b174f73
Roope Järvinen <roope.jarvinen@outlook.com>
2024-10-28 13:00:56 +0200

    Pyinstaller testing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """User Interfaces for DPS Control"""
 import sys
+import os
 from yaml import safe_load, YAMLError
 from lib.dps_controller import DPSController
 from ui.dps_cli import DPSCli
@@ -13,7 +14,9 @@ def main():
     # Try reading configuration
     try:
         config_file = 'dps_control.cfg'
-        with open(config_file, 'r') as file:
+        bundle_dir = getattr(sys, '_MEIPASS', os.path.abspath(os.path.dirname(__file__)))
+        path_to_config = os.path.abspath(os.path.join(bundle_dir, config_file))
+        with open(path_to_config, 'r') as file:
             conf = safe_load(file)
     except YAMLError as error:
         print(f'Error parsing configuration file {error}')
